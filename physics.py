2421612a971619d91fe02f6eccea395b18956a3a
Thought-ed <pguerrerocuello@gmail.com>
2026-06-16 09:43:01 -0500

    refactor plotting for actual performance and persistent objects

--- physics.py
+++ physics.py
@@ -18,61 +18,68 @@ steps = 10000
 xs = []
 ys = []
 
-plt.plot(xs, ys)
+
+
+# Figure Setup
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.set_xlim(-10000, 10000)
+ax.set_ylim(-10000, 10000)
+ax.set_aspect('equal')
+
+ax.set_xlabel('x (km)')
+ax.set_ylabel('y (km)')
+ax.set_title('OrbitalRendezvous Simulation (But just one for now)')
+
+#Earth
+earth = plt.Circle(
+    (0, 0), 
+    EARTH_RADIUS, 
+    fill=True, 
+    color='blue', 
+    alpha=0.5
+)
+
+ax.add_patch(earth)
+ax.annotate(
+    "Earth",
+    (0, 0),
+    xytext=(0, 0),
+    textcoords="offset points"
+)
+
+# Orbit Trail
+orbit_line, = ax.plot([], [], lw=1)
+
+# Satellite Marker
+satellite_dot, = ax.plot([], [], 'ko', markersize=5)
+
+satellite_label = ax.annotate(
+    "Satellite (LEO)", 
+    (x, y),
+    xytext=(10, 10),
+    textcoords='offset points'
+)
+
+plt.show(block=False)
 
 for step in range (steps):
     # calculate acceleration
     r = np.sqrt(x*x + y*y)
-    ax = -MU * x / r**3
-    ay = -MU * y / r**3
-    vx += ax * dt
-    vy += ay * dt
+    ax_grav = -MU * x / r**3
+    ay_grav = -MU * y / r**3
+    vx += ax_grav * dt
+    vy += ay_grav * dt
     x += vx * dt
     y += vy * dt
     xs.append(x)
     ys.append(y)
     
-    if step % 30 == 0:
-        # plotting
-        plt.clf()
-        plt.plot(xs, ys)
-
-        # earth display
-        earth = plt.Circle(
-            (0, 0), 
-            EARTH_RADIUS, 
-            fill=True, 
-            color='blue', 
-            alpha=0.5
-    )
-        plt.annotate(
-            "Earth",
-            (0, 0),
-            xytext=(0, 0),
-            textcoords="offset points"
-        )
-
-        # satellite display
-        plt.scatter(x, y, s=100, c='black')
-        plt.annotate(
-            "Satellite (LEO)", 
-            (x, y),
-            xytext=(10, 10),
-            textcoords = 'offset points'
-            )
-    
-        plt.gca().add_patch(earth)    
-        plt.gca().set_aspect('equal')    
-        plt.xlim(-10000, 10000)
-        plt.ylim(-10000, 10000)
-
-        plt.xlabel('x (km)')
-        plt.ylabel('y (km)')
-        plt.title('OrbitalRendezvous Simulation (But just one for now)')
-
+    if step % 10 == 0:
+        orbit_line.set_data(xs, ys)
+        satellite_dot.set_data([x], [y])
+        satellite_label.xy = (x, y)
+        fig.canvas.draw_idle()
         plt.pause(0.01)
 
-
-plt.legend()
 plt.show()
 
